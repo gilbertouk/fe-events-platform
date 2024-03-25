823ed680e8d780d30153777b165ccfbeb6a23db4
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-03-25 15:24:36 +0000

    feat: :sparkles: Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  
+
+  return (
+    <>
+    <Button>
+      Click me
+    </Button>
+    </>
+  )
+}
+
+export default App
