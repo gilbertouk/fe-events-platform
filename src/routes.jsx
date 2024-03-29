303d57e26896b6159bf3b0b06429d86fa21d1a4c
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-03-29 15:45:28 +0000

    feat: :sparkles: add Home page component

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
+
+import Home from './pages/Home';
 import PageNotFound from './pages/PagNotFound';
+
+import ScrollToTop from './components/ScrollToTop';
+import MenuNavBar from './components/MenuNavBar';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+      <MenuNavBar />
+
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
