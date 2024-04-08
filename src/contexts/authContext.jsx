411aba5dc2b268a166d11cda02b7a4697a7a3fb2
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-04-08 15:46:56 +0100

    feat: :sparkles: add onAuthStateChanged to AuthContext to get logged user

--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -1,14 +1,36 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+
+import { onAuthStateChanged } from 'firebase/auth';
+import auth from '../config/firebase';
 
 export const AuthContext = createContext(null);
 AuthContext.displayName = 'Auth Context';
 
 const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
-  // const [isLoadingUser, setIsLoadingUser] = useState(true);
+  const [isLoadingUser, setIsLoadingUser] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUser(user);
+        setIsLoadingUser(false);
+      } else {
+        setCurrentUser(null);
+        setIsLoadingUser(false);
+        localStorage.clear();
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ currentUser, setCurrentUser }}>
+    <AuthContext.Provider
+      value={{ currentUser, setCurrentUser, isLoadingUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
