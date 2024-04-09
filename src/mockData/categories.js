36db04b167195079a995749dc223b2d06bf6944d
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-04-09 23:17:25 +0100

    test: add mock data

---
+++ src/mockData/categories.js
@@ -0,0 +1,14 @@
+export const categoriesExamples = [
+  { name: 'Fashion', icon: 'Shirt' },
+  { name: 'Arts & Culture', icon: 'PenTool' },
+  { name: 'LGBTQ+', icon: 'Rainbow' },
+  { name: 'Comedy', icon: 'Laugh' },
+  { name: 'Music', icon: 'Music' },
+  { name: 'Sports', icon: 'Medal' },
+  { name: 'Food & Drink', icon: 'UtensilsCrossed' },
+  { name: 'Literature', icon: 'BookOpen' },
+  { name: 'Science', icon: 'Atom' },
+  { name: 'Film', icon: 'Clapperboard' },
+  { name: 'Festival', icon: 'PartyPopper' },
+  { name: 'Documentary', icon: 'FileVideo2' },
+];
---
+++ src/mockData/events.js
@@ -0,0 +1,242 @@
+export const eventsExamples = [
+  {
+    id: 'f97233c2-4569-4e7e-a373-3a3e6d525b3f',
+    name: 'London Fashion Week',
+    date: 'September 14, 2024',
+    location: 'London',
+    category: 'Fashion',
+    price: '60.90',
+    description:
+      'London Fashion Week is a bi-annual event showcasing the latest trends in fashion. Designers from all over the world gather in London to present their collections, which often set the tone for upcoming fashion seasons. The event not only attracts fashion industry professionals such as buyers, stylists, and journalists but also celebrities and fashion enthusiasts. It includes runway shows, presentations, exhibitions, and parties, offering a comprehensive view of the current state of fashion.',
+    information: 'For more information, visit londonfashionweek.com',
+  },
+  {
+    id: '0ec0855e-f868-4e67-ae6d-d70ff16dbb0a',
+    name: 'Edinburgh Fringe Festival',
+    date: 'August 2, 2024',
+    location: 'Edinburgh',
+    category: 'Arts & Culture',
+    price: '26.90',
+    description:
+      "The Edinburgh Fringe Festival is the world's largest arts festival, held annually in Scotland's capital. It features thousands of performances across various art forms, including theatre, comedy, dance, music, and spoken word. The festival is renowned for its open-access policy, allowing anyone with a show to perform. It offers a platform for both established artists and emerging talent to showcase their work to a diverse and international audience. Alongside performances, the festival also hosts workshops, panel discussions, and events, making it a vibrant celebration of creativity and culture.",
+    information: 'For more information, visit edfringe.com',
+  },
+  {
+    id: 'd7d005e0-0670-4fa8-81fd-3a2a1a930379',
+    name: 'Manchester International Festival',
+    date: 'July 4, 2024',
+    location: 'Manchester',
+    category: 'Arts & Culture',
+    price: '17.99',
+    description:
+      "The Manchester International Festival is a biennial arts festival that takes place in various venues across Manchester. It celebrates innovative and cutting-edge performances in arts and culture, showcasing new commissions and collaborations by leading artists from around the world. The festival's program includes a diverse range of events spanning theatre, music, dance, visual arts, and digital innovation. It aims to engage audiences in unique and immersive experiences, fostering creativity and pushing artistic boundaries.",
+    information: 'For more information, visit mif.co.uk',
+  },
+  {
+    id: '3d4c107a-f0e6-4edf-96c4-214bc7c8a634',
+    name: 'Birmingham Pride',
+    date: 'May 25, 2024',
+    location: 'Birmingham',
+    category: 'LGBTQ+',
+    price: 'Free',
+    description:
+      "Birmingham Pride is one of the UK's largest LGBTQ+ celebrations, held annually in the city of Birmingham. It promotes equality, diversity, and inclusivity, celebrating the LGBTQ+ community's achievements and contributions. The event features a vibrant parade through the city streets, followed by a festival in the city's gay village. The festival offers live music, drag performances, dance tents, cabaret acts, community stalls, and more. It provides a safe and welcoming space for people of all genders, sexual orientations, and identities to come together and celebrate.",
+    information: 'For more information, visit birminghampride.com',
+  },
+  {
+    id: '9db84ac2-2de1-499d-baeb-5e31b6f0a0e2',
+    name: 'Glasgow International Comedy Festival',
+    date: 'March 14, 2024',
+    location: 'Glasgow',
+    category: 'Comedy',
+    price: '58.99',
+    description:
+      "The Glasgow International Comedy Festival is one of Europe's largest comedy festivals, showcasing top comedians from around the world. Held annually in the city of Glasgow, the festival features a diverse program of stand-up comedy, sketch shows, improv performances, and comedy theatre. It takes place in various venues across the city, including theatres, clubs, pubs, and even pop-up comedy tents. The festival attracts comedy fans of all ages and tastes, offering something for everyone, from established names to up-and-coming talent.",
+    information: 'For more information, visit glasgowcomedyfestival.com',
+  },
+  {
+    id: '5905b70a-6e97-4d9e-8633-1d11ef31f3fc',
+    name: 'Liverpool Sound City',
+    date: 'May 3, 2024',
+    location: 'Liverpool',
+    category: 'Music',
+    price: '27.90',
+    description:
+      'Liverpool Sound City is an annual music festival and industry conference held in Liverpool. It showcases emerging and established artists from various genres, including indie rock, electronic, hip hop, and more. The festival takes place in venues across the city, including clubs, bars, theatres, and outdoor stages. In addition to live music performances, Sound City hosts networking events, industry panels, workshops, and keynote speeches, providing opportunities for artists, industry professionals, and music enthusiasts to connect and collaborate.',
+    information: 'For more information, visit liverpoolsoundcity.co.uk',
+  },
+  {
+    id: 'f914c7dd-1bf9-4a3b-80bc-cba9cd8b53af',
+    name: 'Brighton Marathon',
+    date: 'April 14, 2024',
+    location: 'Brighton',
+    category: 'Sports',
+    price: '10.99',
+    description:
+      "The Brighton Marathon is an annual long-distance running event held in the coastal city of Brighton, England. It is one of the UK's most popular marathons, attracting thousands of participants from around the world. The scenic route takes runners through the vibrant streets of Brighton and Hove, along the stunning seafront, and past iconic landmarks. The event not only offers a challenging and rewarding race experience but also provides a festive atmosphere with live music, cheering spectators, and community support.",
+    information: 'For more information, visit brightonmarathon.co.uk',
+  },
+  {
+    id: 'ea2a636f-d6f1-47c8-bb25-cd16c1d6f2c8',
+    name: 'Belfast International Arts Festival',
+    date: 'October 11, 2024',
+    location: 'Belfast',
+    category: 'Arts & Culture',
+    price: '72.90',
+    description:
+      'The Belfast International Arts Festival is an annual multidisciplinary arts festival held in the city of Belfast, Northern Ireland. It celebrates creativity and culture through a diverse program of performances, exhibitions, installations, and events. The festival showcases local, national, and international talent across various art forms, including theatre, dance, visual arts, music, and film. It aims to inspire and engage audiences, fostering cultural exchange and dialogue.',
+    information:
+      'For more information, visit belfastinternationalartsfestival.com',
+  },
+  {
+    id: '44f04016-bf2e-4a76-96f5-620bd5e1f932',
+    name: 'Leeds Festival',
+    date: 'August 23, 2024',
+    location: 'Leeds',
+    category: 'Music',
+    price: '30.00',
+    description:
+      "Leeds Festival is one of the UK's premier music festivals, held annually over the August bank holiday weekend. It features a diverse lineup of top bands and artists from around the world, spanning genres such as rock, indie, pop, hip hop, and electronic music. The festival takes place at Bramham Park in Leeds, offering multiple stages, camping facilities, and a range of food and drink options. In addition to music, the festival also includes comedy performances, cinema screenings, and other entertainment, making it a must-attend event for music lovers.",
+    information: 'For more information, visit leedsfestival.com',
+  },
+  {
+    id: 'b5e27e90-47ec-4cc7-b69e-b221462e2a3e',
+    name: 'Cardiff International Food and Drink Festival',
+    date: 'July 5, 2024',
+    location: 'Cardiff',
+    category: 'Food & Drink',
+    price: '18.00',
+    description:
+      'The Cardiff International Food and Drink Festival is an annual culinary event held in the Welsh capital of Cardiff. It showcases the best of Welsh and international cuisine, featuring food stalls, pop-up restaurants, cooking demonstrations, and tasting sessions. The festival celebrates the rich culinary heritage of Wales while also embracing global food trends and flavors. It provides a vibrant and inclusive environment for food lovers to explore, indulge, and enjoy.',
+    information: 'For more information, visit cardifffoodfestival.com',
+  },
+  {
+    id: 'fd9b6a22-52e4-47af-b7d8-989b7db8cc49',
+    name: 'Oxford Literary Festival',
+    date: 'March 30, 2024',
+    location: 'Oxford',
+    category: 'Literature',
+    price: '9.90',
+    description:
+      'The Oxford Literary Festival is an annual celebration of literature and ideas held in the historic city of Oxford, England. It brings together authors, poets, journalists, scholars, and readers from around the world for a diverse program of talks, readings, panel discussions, and literary events. The festival covers a wide range of topics and genres, including fiction, non-fiction, poetry, history, politics, and science. It provides a platform for intellectual exchange, creative exploration, and literary discovery.',
+    information: 'For more information, visit oxfordliteraryfestival.org',
+  },
+  {
+    id: 'd631891b-8eb5-4820-80b4-37d5dce3a251',
+    name: 'Cambridge Science Festival',
+    date: 'March 11, 2024',
+    location: 'Cambridge',
+    category: 'Science',
+    price: '150.90',
+    description:
+      'The Cambridge Science Festival is an annual public event showcasing the wonders of science, technology, engineering, and mathematics (STEM). It takes place in the historic city of Cambridge, home to the world-renowned University of Cambridge and its cutting-edge research institutions. The festival features interactive exhibits, hands-on activities, talks, demonstrations, workshops, and tours, engaging people of all ages and backgrounds in the excitement of scientific discovery. It aims to inspire curiosity, spark creativity, and promote scientific literacy.',
+    information: 'For more information, visit cambridgesciencefestival.org',
+  },
+  {
+    id: '68e91d5e-8e1a-4902-8645-394f9e56d899',
+    name: 'Newcastle International Film Festival',
+    date: 'March 29, 2024',
+    location: 'Newcastle upon Tyne',
+    category: 'Film',
+    price: '130.99',
+    description:
+      'The Newcastle International Film Festival is an annual celebration of independent and international cinema held in the city of Newcastle upon Tyne, England. It showcases a diverse selection of films from around the world, including feature films, documentaries, shorts, and experimental works. The festival provides a platform for filmmakers to showcase their creativity, share their stories, and connect with audiences. In addition to screenings, the festival also hosts Q&A sessions, industry panels, workshops, and networking events, fostering collaboration and dialogue within the film community.',
+    information: 'For more information, visit newcastlefilmfestival.org',
+  },
+  {
+    id: '1a6c9383-cf2c-40dc-9f44-b47aa80bdc87',
+    name: 'York Chocolate Festival',
+    date: 'April 12, 2024',
+    location: 'York',
+    category: 'Food & Drink',
+    price: 'Free',
+    description:
+      'The York Chocolate Festival is an annual celebration of all things chocolate held in the historic city of York, England. It showcases the finest chocolates from local artisans and international brands, offering tastings, demonstrations, workshops, and chocolate-themed events. The festival celebrates the rich history and cultural significance of chocolate, from its ancient origins to modern innovations. It provides a sweet and indulgent experience for chocolate lovers of all ages.',
+    information: 'For more information, visit yorkchocolatefestival.com',
+  },
+  {
+    id: '888c8fe5-3b6b-41d3-8cfd-963c024611fb',
+    name: 'Bristol Balloon Fiesta',
+    date: 'August 8, 2024',
+    location: 'Bristol',
+    category: 'Festival',
+    price: 'Free',
+    description:
+      "The Bristol Balloon Fiesta is an annual hot air balloon festival held in the city of Bristol, England. It is one of Europe's largest ballooning events, attracting hundreds of hot air balloons and thousands of spectators from around the world. The fiesta features mass ascents of balloons at dawn and dusk, illuminated night glows, air displays, and family entertainment. It offers a spectacular sight against the backdrop of Bristol's iconic landmarks, including the Clifton Suspension Bridge and the Avon Gorge.",
+    information: 'For more information, visit bristolballoonfiesta.org',
+  },
+  {
+    id: 'f47391e1-f36d-4e32-88c0-0b1e49cb4390',
+    name: 'Sheffield Doc/Fest',
+    date: 'June 6, 2024',
+    location: 'Sheffield',
+    category: 'Documentary',
+    price: '78.00',
+    description:
+      "Sheffield Doc/Fest is an annual documentary film festival held in the city of Sheffield, England. It is one of the UK's leading documentary festivals, showcasing non-fiction storytelling through film screenings, interactive exhibits, live events, and industry conferences. The festival features a diverse selection of documentary films from around the world, covering a wide range of subjects and perspectives. It provides a platform for filmmakers, industry professionals, and audiences to explore pressing issues, share diverse stories, and engage in dialogue.",
+    information: 'For more information, visit sheffielddocfest.com',
+  },
+];
+
+export const trendingEventsExamples = [
+  {
+    id: 'f97233c2-4569-4e7e-a373-3a3e6d525b3f',
+    name: 'London Fashion Week',
+    date: 'September 14, 2024',
+    location: 'London',
+    category: 'Fashion',
+    price: '60.90',
+    description:
+      'London Fashion Week is a bi-annual event showcasing the latest trends in fashion.',
+  },
+  {
+    id: '0ec0855e-f868-4e67-ae6d-d70ff16dbb0a',
+    name: 'Edinburgh Fringe Festival',
+    date: 'August 2, 2024',
+    location: 'Edinburgh',
+    category: 'Arts & Culture',
+    price: '26.90',
+    description:
+      "The Edinburgh Fringe Festival is the world's largest arts festival, featuring thousands of performances across various art forms.",
+  },
+  {
+    id: 'd7d005e0-0670-4fa8-81fd-3a2a1a930379',
+    name: 'Manchester International Festival',
+    date: 'July 4, 2024',
+    location: 'Manchester',
+    category: 'Arts & Culture',
+    price: '17.99',
+    description:
+      'The Manchester International Festival celebrates innovative and cutting-edge performances in arts and culture.',
+  },
+  {
+    id: '3d4c107a-f0e6-4edf-96c4-214bc7c8a634',
+    name: 'Birmingham Pride',
+    date: 'May 25, 2024',
+    location: 'Birmingham',
+    category: 'LGBTQ+',
+    price: 'Free',
+    description:
+      "Birmingham Pride is one of the UK's largest LGBTQ+ celebrations, promoting equality and diversity.",
+  },
+  {
+    id: '9db84ac2-2de1-499d-baeb-5e31b6f0a0e2',
+    name: 'Glasgow International Comedy Festival',
+    date: 'March 14, 2024',
+    location: 'Glasgow',
+    category: 'Comedy',
+    price: '58.99',
+    description:
+      'The Glasgow International Comedy Festival features top comedians from around the world, bringing laughter to the city.',
+  },
+  {
+    id: '5905b70a-6e97-4d9e-8633-1d11ef31f3fc',
+    name: 'Liverpool Sound City',
+    date: 'May 3, 2024',
+    location: 'Liverpool',
+    category: 'Music',
+    price: '27.90',
+    description:
+      'Liverpool Sound City is a music festival showcasing emerging and established artists in various venues across the city.',
+  },
+];
