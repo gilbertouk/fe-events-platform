fd503e89d40cb8245ab33e76d9a03c0e8febbdd9
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-04-10 12:27:53 +0100

    refactor: :recycle: remove comment from Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -78,12 +78,9 @@ const LoginPage = () => {
       setCurrentUser(user);
       navigate('/');
     } catch (error) {
-      // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
-      // The email of the user's account used.
       const email = error.customData.email;
-      // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
 
       console.log(errorCode, errorMessage, email, credential);
