4cfe0b6b7b2a0db61da21042003969c8983de931
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-04-10 23:27:23 +0100

    feat: :sparkles: create Terms page

---
+++ src/pages/Terms.jsx
@@ -0,0 +1,84 @@
+const TermsPage = () => {
+  return (
+    <main className="w-full py-12 bg-gray-100">
+      <div className="m-auto flex justify-center flex-col max-w-3xl gap-6 px-4">
+        <h1 className="font-roboto text-3xl font-bold tracking-tighter sm:text-5xl pb-4">
+          Terms and Conditions of Use
+        </h1>
+        <p className="text-gray-500 text-left sm:text-justify">
+          Please read these Terms and Conditions of Use carefully before using
+          the EVENTS PLATFORM.
+        </p>
+        <p className="text-gray-500 text-left sm:text-justify">
+          Your access to and use of the Platform is conditioned on your
+          acceptance and compliance with these Terms. These Terms apply to all
+          visitors, users, and other individuals who access or use the Platform.
+        </p>
+        <p className="text-gray-500 text-left sm:text-justify">
+          By accessing or using the Platform, you agree to be bound by these
+          Terms. If you disagree with any part of these Terms, you are not
+          permitted to access the Platform.
+        </p>
+        <h2 className="font-roboto text-xl font-bold tracking-tighter sm:text-2xl">
+          Accounts
+        </h2>
+        <p className="text-gray-500 text-left sm:text-justify">
+          When creating an account with us, you must provide accurate, complete,
+          and up-to-date information. Failure to comply constitutes a breach of
+          the Terms, which may result in immediate termination of your account
+          on our Platform.
+        </p>
+        <p className="text-gray-500 text-left sm:text-justify">
+          You are responsible for safeguarding the password that you use to
+          access the Platform and for any activities or actions performed under
+          your account. You agree not to disclose your password to third
+          parties. You must notify us immediately upon becoming aware of any
+          breach of security or unauthorized use of your account.
+        </p>
+        <h2 className="font-roboto text-xl font-bold tracking-tighter sm:text-2xl">
+          Links to Other Websites
+        </h2>
+        <p className="text-gray-500 text-left sm:text-justify">
+          Our Platform may contain links to third-party websites or services
+          that are not owned or controlled by us.
+        </p>
+        <p className="text-gray-500 text-left sm:text-justify">
+          We have no control over the content, privacy policies, or practices of
+          any third-party websites or services and assume no responsibility for
+          them.
+        </p>
+        <h2 className="font-roboto text-xl font-bold tracking-tighter sm:text-2xl">
+          Acceptable Use
+        </h2>
+        <p className="text-gray-500 text-left sm:text-justify">
+          You agree to use the Platform only for lawful purposes and in a manner
+          that does not infringe the rights of third parties, nor restrict or
+          inhibit the use and enjoyment of the Platform by any person.
+        </p>
+        <h2 className="font-roboto text-xl font-bold tracking-tighter sm:text-2xl">
+          Changes
+        </h2>
+        <p className="text-gray-500 text-left sm:text-justify">
+          We reserve the right, at our sole discretion, to modify or replace
+          these Terms at any time. If a revision is significant, we will try to
+          provide at least 30 days&apos; notice prior to any new terms taking
+          effect. What constitutes a significant change will be determined at
+          our sole discretion.
+        </p>
+        <p className="text-gray-500 text-left sm:text-justify">
+          By continuing to access or use our Platform after those revisions
+          become effective, you agree to be bound by the revised terms. If you
+          do not agree to the new terms, please stop using the Platform.
+        </p>
+        <h2 className="font-roboto text-xl font-bold tracking-tighter sm:text-2xl">
+          Contact Us
+        </h2>
+        <p className="text-gray-500 text-left">
+          If you have any questions about these Terms, please contact us.
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default TermsPage;
