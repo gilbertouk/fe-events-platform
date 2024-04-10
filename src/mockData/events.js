4cba41758e88385efee5d51d601fd98c45787a7c
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-04-10 11:39:24 +0100

    chore: update events mockData

--- src/mockData/events.js
+++ src/mockData/events.js
@@ -176,6 +176,84 @@ export const eventsExamples = [
       "Sheffield Doc/Fest is an annual documentary film festival held in the city of Sheffield, England. It is one of the UK's leading documentary festivals, showcasing non-fiction storytelling through film screenings, interactive exhibits, live events, and industry conferences. The festival features a diverse selection of documentary films from around the world, covering a wide range of subjects and perspectives. It provides a platform for filmmakers, industry professionals, and audiences to explore pressing issues, share diverse stories, and engage in dialogue.",
     information: 'For more information, visit sheffielddocfest.com',
   },
+  {
+    id: '8a826df8-6f80-4f27-b2b7-06ad886c8d1a',
+    name: 'Cardiff International Food and Drink Festival',
+    date: 'July 5, 2024',
+    location: 'Cardiff',
+    category: 'Food & Drink',
+    price: '£18.00',
+    description:
+      'The Cardiff International Food and Drink Festival showcases the best of Welsh and international cuisine, featuring food stalls, pop-up restaurants, cooking demonstrations, and tasting sessions. The festival celebrates the rich culinary heritage of Wales while also embracing global food trends and flavors. It provides a vibrant and inclusive environment for food lovers to explore, indulge, and enjoy.',
+    information: 'For more information, visit cardifffoodfestival.com',
+  },
+  {
+    id: 'd6b7fc69-4847-4d2b-af21-b443795a2b67',
+    name: 'York Chocolate Festival',
+    date: 'April 12, 2024',
+    location: 'York',
+    category: 'Food & Drink',
+    price: 'Free',
+    description:
+      'The York Chocolate Festival is an annual celebration of all things chocolate held in the historic city of York, England. It showcases the finest chocolates from local artisans and international brands, offering tastings, demonstrations, workshops, and chocolate-themed events. The festival celebrates the rich history and cultural significance of chocolate, from its ancient origins to modern innovations. It provides a sweet and indulgent experience for chocolate lovers of all ages.',
+    information: 'For more information, visit yorkchocolatefestival.com',
+  },
+  {
+    id: '2a5687de-7730-4725-83f8-faf3a5ccce19',
+    name: 'London Coffee Festival',
+    date: 'May 18, 2024',
+    location: 'London',
+    category: 'Food & Drink',
+    price: '£25.00',
+    description:
+      "The London Coffee Festival is Europe's largest gathering of coffee enthusiasts, featuring artisan coffee, gourmet food stalls, barista competitions, and interactive workshops. Coffee lovers can sample a wide variety of specialty coffees from around the world, learn about coffee brewing techniques, and discover the latest trends in the coffee industry. The festival offers a lively atmosphere with live music, DJs, and coffee-inspired art installations.",
+    information: 'For more information, visit londoncoffeefestival.com',
+  },
+  {
+    id: '29d9d7cf-2714-4b6c-87b5-5de9c42898d6',
+    name: 'Bristol Food Connections',
+    date: 'June 2, 2024',
+    location: 'Bristol',
+    category: 'Food & Drink',
+    price: 'Varies',
+    description:
+      "Bristol Food Connections is a citywide festival celebrating Bristol's vibrant food culture and community. It features a diverse program of events, including food tastings, cooking demonstrations, workshops, talks, and food-themed walking tours. The festival highlights local producers, chefs, and food entrepreneurs, showcasing the best of Bristol's culinary scene. It also explores important food-related topics such as sustainability, food waste reduction, and community food initiatives.",
+    information: 'For more information, visit bristolfoodconnections.com',
+  },
+  {
+    id: 'a4baf76e-92d1-48cb-afeb-96a0575a4e62',
+    name: 'Edinburgh Fringe Festival',
+    date: 'August 2, 2024',
+    location: 'Edinburgh',
+    category: 'Arts & Culture',
+    price: '£26.90',
+    description:
+      "The Edinburgh Fringe Festival is the world's largest arts festival, featuring thousands of performances across various art forms. It takes place annually in Edinburgh, Scotland, and encompasses theatre, comedy, dance, music, and more. The festival offers a platform for both established and emerging artists to showcase their talents, with performances ranging from experimental avant-garde theatre to traditional storytelling. It attracts artists and audiences from around the globe, making it a vibrant celebration of creativity and culture.",
+    information: 'For more information, visit edfringe.com',
+  },
+  {
+    id: '5b6206f5-067b-42e4-b42c-bd8229b1d5ff',
+    name: 'Manchester International Festival',
+    date: 'July 4, 2024',
+    location: 'Manchester',
+    category: 'Arts & Culture',
+    price: '£17.99',
+    description:
+      'The Manchester International Festival celebrates innovative and cutting-edge performances in arts and culture. Held biennially in Manchester, England, the festival showcases a diverse range of art forms including visual arts, theatre, music, and dance. It commissions and presents new work by artists from around the world, pushing the boundaries of creativity and exploring contemporary issues. With a mix of world premieres, large-scale productions, and intimate performances, the festival attracts audiences seeking unique and thought-provoking cultural experiences.',
+    information: 'For more information, visit mif.co.uk',
+  },
+  {
+    id: '408896e0-8507-4b65-9a79-1d0d9d871992',
+    name: 'Belfast International Arts Festival',
+    date: 'October 11, 2024',
+    location: 'Belfast',
+    category: 'Arts & Culture',
+    price: '£72.90',
+    description:
+      'The Belfast International Arts Festival is an annual multidisciplinary arts festival held in the city of Belfast, Northern Ireland. It celebrates creativity and culture through a diverse program of performances, exhibitions, installations, and events. The festival showcases local, national, and international talent across various art forms, including theatre, dance, visual arts, music, and film. It aims to inspire and engage audiences, fostering cultural exchange and dialogue.',
+    information:
+      'For more information, visit belfastinternationalartsfestival.com',
+  },
 ];
 
 export const trendingEventsExamples = [
