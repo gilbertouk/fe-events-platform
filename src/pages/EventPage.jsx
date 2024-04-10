9982bfb1755a32303f5d99e879c806a43ae13815
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-04-10 10:33:12 +0100

    style: :lipstick: update EventPage css

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -25,61 +25,63 @@ const EventPage = () => {
   if (loading) {
     return (
       <div className="w-full h-full flex items-center justify-center">
-        <div className="w-12 h-12 bg-gray-200 rounded-full animate-pulse"></div>
+        <div className="w-12 h-12 bg-gray-100 rounded-full animate-pulse"></div>
       </div>
     );
   }
 
   return (
-    <main className="max-w-screen-xl w-auto mx-auto mb-12 lg:mb-8">
-      <section>
-        <AspectRatio ratio={3 / 1} className="bg-muted">
-          <img
-            alt="Event banner"
-            src="/event.png"
-            className="w-full h-full object-cover rounded-sm rounded-t-none"
-          />
-        </AspectRatio>
-      </section>
-      <section className="flex flex-col mt-4 gap-4 lg:flex-row m-3 lg:m-0 lg:mt-3">
-        <div className="my-8 text-start shadow-xl shadow-gray-300 p-3">
-          <h1 className="font-roboto text-center font-bold mb-6 text-lg sm:text-2xl lg:text-4xl sm:mb-8 lg:mb-12">
-            {event.name}
-          </h1>
-          <h2 className="font-roboto font-bold text-base sm:text-xl lg:text-2xl">
-            Event Description
-          </h2>
-          <p className="text-justify text-sm mt-3 sm:text-base sm:mt-6 text-slate-500">
-            {event.description}
-          </p>
-          <h2 className="font-roboto font-bold text-base sm:text-xl lg:text-2xl mt-3 sm:mt-6">
-            Event Information
-          </h2>
-          <p className="text-justify text-sm mt-3 sm:text-base sm:mt-6 text-slate-500">
-            {event.information}
-          </p>
-        </div>
-        <div className="shadow-xl shadow-gray-300 p-3 h-fit">
-          <h2 className="font-roboto font-bold text-base sm:text-xl lg:text-2xl sm:mt-6">
-            Event Detail
-          </h2>
-          <p className="text-justify text-sm mt-3 sm:text-base sm:mt-6 text-slate-500 flex gap-3 justify-start items-center">
-            <CalendarDays /> {event.date}
-          </p>
-          <p className="text-justify text-sm mt-3 sm:text-base sm:mt-6 text-slate-500 flex gap-3 justify-start items-center">
-            <MapPin /> {event.location}
-          </p>
-          <p className="text-justify text-sm mt-3 sm:text-base sm:mt-6 text-slate-500 flex gap-3 justify-start items-center">
-            &nbsp;
-            {event.price === 'Free' ? `${event.price}` : `£ ${event.price}`}
-          </p>
-          <hr className="my-4" />
-          <div className="flex flex-row justify-between items-center mt-3 gap-1 lg:gap-4">
-            <Button className="w-24 sm:w-32">Buy Now</Button>
-            <Button className="w-auto">Add to Calendar</Button>
+    <main className="bg-gray-100">
+      <div className="max-w-screen-xl w-auto mx-auto">
+        <section className="mb-3">
+          <AspectRatio ratio={3 / 1} className="bg-muted">
+            <img
+              alt="Event banner"
+              src="/event.png"
+              className="w-full h-full object-cover rounded-sm rounded-t-none"
+            />
+          </AspectRatio>
+        </section>
+        <section className="flex flex-col gap-4 lg:flex-row py-8 mx-3 xl:mx-0">
+          <div className="text-start shadow-xl shadow-gray-300 bg-white p-4 xl:p-8">
+            <h1 className="font-roboto text-center font-bold mb-6 text-lg sm:text-2xl lg:text-4xl sm:mb-8 lg:mb-12">
+              {event.name}
+            </h1>
+            <h2 className="font-roboto font-bold text-base sm:text-xl lg:text-2xl">
+              Event Description
+            </h2>
+            <p className="text-justify text-sm mt-3 sm:text-base sm:mt-6 text-slate-500">
+              {event.description}
+            </p>
+            <h2 className="font-roboto font-bold text-base sm:text-xl lg:text-2xl mt-3 sm:mt-6">
+              Event Information
+            </h2>
+            <p className="text-justify text-sm mt-3 sm:text-base sm:mt-6 text-slate-500">
+              {event.information}
+            </p>
           </div>
-        </div>
-      </section>
+          <div className="shadow-xl shadow-gray-300 h-fit bg-white p-4 xl:p-8">
+            <h2 className="font-roboto font-bold text-base sm:text-xl lg:text-2xl">
+              Event Detail
+            </h2>
+            <p className="text-justify text-sm mt-3 sm:text-base sm:mt-6 text-slate-500 flex gap-3 justify-start items-center">
+              <CalendarDays /> {event.date}
+            </p>
+            <p className="text-justify text-sm mt-3 sm:text-base sm:mt-6 text-slate-500 flex gap-3 justify-start items-center">
+              <MapPin /> {event.location}
+            </p>
+            <p className="text-justify text-sm mt-3 sm:text-base sm:mt-6 text-slate-500 flex gap-3 justify-start items-center">
+              &nbsp;
+              {event.price === 'Free' ? `${event.price}` : `£ ${event.price}`}
+            </p>
+            <hr className="my-4" />
+            <div className="flex flex-row justify-between items-center mt-3 gap-1 lg:gap-4">
+              <Button className="w-24 sm:w-32">Buy Now</Button>
+              <Button className="w-auto">Add to Calendar</Button>
+            </div>
+          </div>
+        </section>
+      </div>
     </main>
   );
 };
