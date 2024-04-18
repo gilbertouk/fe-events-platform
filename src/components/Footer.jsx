97aa17841fb42d6d4a3b975c12d9d81cc586b2d9
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-04-18 14:15:49 +0100

    refactor: :recycle: change single quotes to double

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import Icon from './icons/Icon';
+import { Link } from "react-router-dom";
+import Icon from "./icons/Icon";
 
 const Footer = () => {
   const getYear = () => {
